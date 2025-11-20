bee4ee757ebf9d77c9f107db9e891fcff560edaa
Toth Balint <toth.balint@bolyai.local>
2025-11-20 13:58:28 +0100

    navigáció kialakítása

---
+++ frontend/budget_buddy/src/Navigacio.jsx
@@ -0,0 +1,14 @@
+import logo from "./img/logo.png"
+
+
+export function Navigacio(){
+    return(
+        <div className="navigacio">
+            <p id="logo"> 
+                <img src={logo} alt="logo"></img>
+            </p>
+            <p>asdasd</p>
+            <p>dsadasd</p>
+        </div>
+    )
+}
