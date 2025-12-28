149dfbfa9235242399fcd26b04053766040a3efd
SigmaSpeedy9 <tothbalint249@gmail.com>
2025-12-28 23:50:54 +0100

    fetch hívások megírása

--- frontend/budget_buddy/src/fetch.js
+++ frontend/budget_buddy/src/fetch.js
@@ -16,6 +16,25 @@ export function login(email, password) {
     });
 }
 
+export function register(name, email, password) {
+  return fetch("http://localhost:3000/auth/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.log("Hiba:", err);
+    });
+}
+
 export function getBalance(userId) {
   return fetch(`http://localhost:3000/transactions/balance/${userId}`)
     .then((res) => res.json())
@@ -57,6 +76,36 @@ export function getExpensesByCategory(userId) {
 
 export function getGoals(userId) {
   return fetch(`http://localhost:3000/goals/${userId}`)
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.log("Hiba:", err);
+    });
+}
+
+export function updateUser(userId, name, email, password) {
+  return fetch(`http://localhost:3000/users/${userId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+      password: password,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => data)
+    .catch((err) => {
+      console.log("Hiba:", err);
+    });
+}
+
+export function deleteUser(userId) {
+  return fetch(`http://localhost:3000/users/${userId}`, {
+    method: "DELETE",
+  })
     .then((res) => res.json())
     .then((data) => data)
     .catch((err) => {
