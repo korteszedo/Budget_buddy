2010e3e28dae1017849d2cded3545657550e8e79
Alex <galalex20160925@gmail.com>
2026-01-07 20:17:05 +0100

    token

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -3,13 +3,14 @@ import authRoutes from "../auth/authroutes";
 import transactionRoutes from "../transactions/transactionroutes";
 import goalRoutes from "../goals/goalsroutes"
 import userRoutes from "../users/userroutes";
+import verifyToken from "../middleware/verifyToken";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
-router.use("/transactions", transactionRoutes);
-router.use("/goals", goalRoutes);
-router.use("/users", userRoutes);
+router.use("/transactions", verifyToken, transactionRoutes);
+router.use("/goals", verifyToken, goalRoutes);
+router.use("/users", verifyToken, userRoutes);
 
 router.get("/", (_req, res) => res.send("futbazdmeg"));
 
